d3053cb013f8b2a5d7a2cf5df759c494fda6d6ac
Charlie Hess <charlie@slack-corp.com>
2019-10-24 11:03:45 -0400

    Add the observer using KVO.

--- spec/keyboard-layout-spec.js
+++ spec/keyboard-layout-spec.js
@@ -24,4 +24,22 @@ describe('Keyboard Layout', () => {
       })
     })
   }
+
+  describe('onDidChangeCurrentKeyboardLayout', () => {
+    it('should notify when do not disturb changes', function (done) {
+      let secondsLeft = 10
+
+      const interval = setInterval(() => {
+        process.stdout.clearLine()
+        process.stdout.cursorTo(5)
+        process.stdout.write(`Please toggle Do Not Disturb. ${secondsLeft} seconds remaining...`)
+        secondsLeft = secondsLeft - 1
+      }, 1000)
+
+      KeyboardLayout.onDidChangeCurrentKeyboardLayout(() => {
+        clearInterval(interval)
+        done()
+      })
+    }, 10000)
+  })
 })
--- src/keyboard-layout-manager-mac.mm
+++ src/keyboard-layout-manager-mac.mm
@@ -3,6 +3,7 @@
 
 #import <Carbon/Carbon.h>
 #import "keyboard-layout-manager.h"
+#import "do-not-disturb-observer.mm"
 #include <vector>
 #include <string>
 #include <cctype>
@@ -12,36 +13,22 @@ using namespace v8;
 uv_loop_t *loop = uv_default_loop();
 uv_async_t async;
 
-static void notificationHandler(CFNotificationCenterRef center, void *manager, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
-  async.data = manager;
-  uv_async_send(&async);
-}
-
 static void asyncSendHandler(uv_async_t *handle) {
   (static_cast<KeyboardLayoutManager *>(handle->data))->HandleKeyboardLayoutChanged();
 }
 
 static void RemoveCFObserver(void *arg) {
   auto manager = static_cast<KeyboardLayoutManager*>(arg);
-  CFNotificationCenterRemoveObserver(
-    CFNotificationCenterGetDistributedCenter(),
-    manager,
-    kTISNotifySelectedKeyboardInputSourceChanged,
-    NULL
-  );
+  // [manager->observer dealloc];
 }
 
 KeyboardLayoutManager::KeyboardLayoutManager(v8::Isolate *isolate, Nan::Callback *callback) : isolate_(isolate), callback(callback) {
   uv_async_init(loop, &async, (uv_async_cb) asyncSendHandler);
 
-  CFNotificationCenterAddObserver(
-      CFNotificationCenterGetDistributedCenter(),
-      this,
-      notificationHandler,
-      kTISNotifySelectedKeyboardInputSourceChanged,
-      NULL,
-      CFNotificationSuspensionBehaviorDeliverImmediately
-  );
+  auto observer = [[DoNotDisturbObserver alloc] initWithCallback:^() {
+    async.data = this;
+    uv_async_send(&async);
+  }];
 
 #if NODE_MAJOR_VERSION >= 10
   node::AddEnvironmentCleanupHook(
